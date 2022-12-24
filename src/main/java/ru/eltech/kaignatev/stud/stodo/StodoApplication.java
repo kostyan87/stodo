aff9e451d60188737d6660389dadd69953b26bfa
Konstantin <kostya.ignatev.14@mail.ru>
2022-12-24 13:11:34 +0300

    Создан начальный проект

---
+++ src/main/java/ru/eltech/kaignatev/stud/stodo/StodoApplication.java
@@ -0,0 +1,13 @@
+package ru.eltech.kaignatev.stud.stodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/eltech/kaignatev/stud/stodo/StodoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.eltech.kaignatev.stud.stodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
